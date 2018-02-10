9589df15a954c868b267afc43ca17bf3a7c0a99a
anas youbi <anasyoubi@gmail.com>
2018-02-10 21:06:19 +0100

    test

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#333333'
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
